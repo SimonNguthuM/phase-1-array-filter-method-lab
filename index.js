613ac72cf259e08198c5f599cb70db3a51ba0821
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-07-01 20:07:43 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(array, string){
+    const string1 = string.toUpperCase()
+    return array.filter(str => str.toUpperCase() === string1)
+}
+function fuzzyMatch(array, string){
+    return array.filter(str => str.startsWith(string))
+}
+function matchName(array, string){
+    return array.filter(obj => obj.name === string)
+}
